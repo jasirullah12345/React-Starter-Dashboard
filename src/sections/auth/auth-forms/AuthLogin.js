34bdd9db056fbefc6a28351329ac4df73c2d5d5b
Haroon Awan <haroonashrafawan@gmail.com>
2023-01-28 13:30:59 +0500

    Fix!

--- src/sections/auth/auth-forms/AuthLogin.js
+++ src/sections/auth/auth-forms/AuthLogin.js
@@ -37,7 +37,7 @@ const AuthLogin = () => {
     const navigate = useNavigate();
     const [credentials, setCredentials] = useLocalStorage('credentials', null);
 
-    const [checked, setChecked] = React.useState(false);
+    const [checked, setChecked] = React.useState(!!credentials);
     const [capsWarning, setCapsWarning] = React.useState(false);
 
     const {login} = useAuth();
@@ -78,6 +78,8 @@ const AuthLogin = () => {
                             () => {
                                 if (checked) {
                                     setCredentials({email: values.email, password: values.password});
+                                } else {
+                                    setCredentials(null);
                                 }
                                 navigate(config.defaultPath, {replace: true});
                             },
@@ -167,30 +169,30 @@ const AuthLogin = () => {
                                 </Stack>
                             </Grid>
 
-                            {/*<Grid item xs={12} sx={{mt: -1}}>*/}
-                            {/*    <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>*/}
-                            {/*        <FormControlLabel*/}
-                            {/*            control={*/}
-                            {/*                <Checkbox*/}
-                            {/*                    checked={checked}*/}
-                            {/*                    onChange={(event) => setChecked(event.target.checked)}*/}
-                            {/*                    name="checked"*/}
-                            {/*                    color="primary"*/}
-                            {/*                    size="small"*/}
-                            {/*                />*/}
-                            {/*            }*/}
-                            {/*            label={<Typography variant="h6">Keep me sign in</Typography>}*/}
-                            {/*        />*/}
-                            {/*        <Link*/}
-                            {/*            variant="h6"*/}
-                            {/*            component={RouterLink}*/}
-                            {/*            to={'/forgot-password'}*/}
-                            {/*            color="text.primary"*/}
-                            {/*        >*/}
-                            {/*            Forgot Password?*/}
-                            {/*        </Link>*/}
-                            {/*    </Stack>*/}
-                            {/*</Grid>*/}
+                            <Grid item xs={12} sx={{mt: -1}}>
+                                <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
+                                    <FormControlLabel
+                                        control={
+                                            <Checkbox
+                                                checked={checked}
+                                                onChange={(event) => setChecked(event.target.checked)}
+                                                name="checked"
+                                                color="primary"
+                                                size="small"
+                                            />
+                                        }
+                                        label={<Typography variant="h6">Keep me sign in</Typography>}
+                                    />
+                                    {/*<Link*/}
+                                    {/*    variant="h6"*/}
+                                    {/*    component={RouterLink}*/}
+                                    {/*    to={'/forgot-password'}*/}
+                                    {/*    color="text.primary"*/}
+                                    {/*>*/}
+                                    {/*    Forgot Password?*/}
+                                    {/*</Link>*/}
+                                </Stack>
+                            </Grid>
                             {errors.submit && (
                                 <Grid item xs={12}>
                                     <FormHelperText error>{errors.submit}</FormHelperText>
