7299403f069e37062f14bf59068a80cac8ce0b0d
Haroon Awan <haroonashrafawan@gmail.com>
2023-01-28 12:34:32 +0500

    Hide signup section

--- src/sections/auth/auth-forms/AuthLogin.js
+++ src/sections/auth/auth-forms/AuthLogin.js
@@ -167,30 +167,30 @@ const AuthLogin = () => {
                                 </Stack>
                             </Grid>
 
-                            <Grid item xs={12} sx={{mt: -1}}>
-                                <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
-                                    <FormControlLabel
-                                        control={
-                                            <Checkbox
-                                                checked={checked}
-                                                onChange={(event) => setChecked(event.target.checked)}
-                                                name="checked"
-                                                color="primary"
-                                                size="small"
-                                            />
-                                        }
-                                        label={<Typography variant="h6">Keep me sign in</Typography>}
-                                    />
-                                    <Link
-                                        variant="h6"
-                                        component={RouterLink}
-                                        to={'/forgot-password'}
-                                        color="text.primary"
-                                    >
-                                        Forgot Password?
-                                    </Link>
-                                </Stack>
-                            </Grid>
+                            {/*<Grid item xs={12} sx={{mt: -1}}>*/}
+                            {/*    <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>*/}
+                            {/*        <FormControlLabel*/}
+                            {/*            control={*/}
+                            {/*                <Checkbox*/}
+                            {/*                    checked={checked}*/}
+                            {/*                    onChange={(event) => setChecked(event.target.checked)}*/}
+                            {/*                    name="checked"*/}
+                            {/*                    color="primary"*/}
+                            {/*                    size="small"*/}
+                            {/*                />*/}
+                            {/*            }*/}
+                            {/*            label={<Typography variant="h6">Keep me sign in</Typography>}*/}
+                            {/*        />*/}
+                            {/*        <Link*/}
+                            {/*            variant="h6"*/}
+                            {/*            component={RouterLink}*/}
+                            {/*            to={'/forgot-password'}*/}
+                            {/*            color="text.primary"*/}
+                            {/*        >*/}
+                            {/*            Forgot Password?*/}
+                            {/*        </Link>*/}
+                            {/*    </Stack>*/}
+                            {/*</Grid>*/}
                             {errors.submit && (
                                 <Grid item xs={12}>
                                     <FormHelperText error>{errors.submit}</FormHelperText>
