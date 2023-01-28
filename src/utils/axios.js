dcea8752ef48487d58a856d1436f6a04a226cf3b
Haroon Awan <haroonashrafawan@gmail.com>
2023-01-28 11:59:36 +0500

    Fix cors issue!

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,9 +3,7 @@ import axios from "axios";
 const API_SERVER_ADDRESS = process.env.REACT_APP_API_SERVER_ADDRESS
 
 const axiosServices = axios.create({
-    baseURL: API_SERVER_ADDRESS,
-    // For Cookies Send and Receive
-    withCredentials: true
+    baseURL: API_SERVER_ADDRESS
 });
 
 export default axiosServices;
