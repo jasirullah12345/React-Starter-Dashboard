4f5a160cd0de6b2a666e34d4baad631c8125c8f0
Haroon Awan <haroonashrafawan@gmail.com>
2023-01-28 11:49:31 +0500

    test

--- src/sections/auth/auth-forms/AuthRegister.js
+++ src/sections/auth/auth-forms/AuthRegister.js
@@ -5,7 +5,6 @@ import { Link as RouterLink } from 'react-router-dom';
 import {
   Box,
   Button,
-  Divider,
   FormControl,
   FormHelperText,
   Grid,
@@ -26,7 +25,6 @@ import useAuth from 'hooks/useAuth';
 import useScriptRef from 'hooks/useScriptRef';
 import IconButton from 'components/@extended/IconButton';
 import AnimateButton from 'components/@extended/AnimateButton';
-import FirebaseSocial from './FirebaseSocial';
 import { strengthColor, strengthIndicator } from 'utils/password-strength';
 
 // assets
@@ -261,14 +259,6 @@ const AuthRegister = () => {
                     Create Account
                   </Button>
                 </AnimateButton>
-              </Grid>
-              <Grid item xs={12}>
-                <Divider>
-                  <Typography variant="caption">Sign up with</Typography>
-                </Divider>
-              </Grid>
-              <Grid item xs={12}>
-                <FirebaseSocial />
               </Grid>
             </Grid>
           </form>
